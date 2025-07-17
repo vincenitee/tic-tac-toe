9b8cfda0da411eb3899769997e555f6b6bb5d79c
Vincent <vincentbolinget@gmail.com>
2025-07-16 21:39:20 -1200

    Add a current player style to the UI

--- app.js
+++ app.js
@@ -159,7 +159,53 @@ function GameController() {
 
 }
 
-const gameController = GameController();
+function ScreenController() {
+    const marks = {
+        'O': `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <title>circle-outline</title>
+                    <path
+                        d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
+                </svg>`,
+        'X': `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <title>window-close</title>
+                    <path
+                        d="M13.46,12L19,17.54V19H17.54L12,13.46L6.46,19H5V17.54L10.54,12L5,6.46V5H6.46L12,10.54L17.54,5H19V6.46L13.46,12Z" />
+                </svg>`,
+    };
+
+    const select = (element) => document.querySelector(element);
+    const selectAll = (element) => document.querySelectorAll(element);
+
+    const boardElement = select('.game-board');
+
+    const gameBoard = Gameboard();
+
+    const updateScreen = () => {
+        const board = gameBoard.getBoard();
+        
+        boardElement.innerHTML = '';
+
+        board.forEach((row, rowIndex) => {
+            console.log(row);
+            row.forEach((cell, collIndex) => {
+                const cellElement = document.createElement('button');
+                cellElement.classList.add('cell');
+                cellElement.dataset.row = rowIndex;
+                cellElement.dataset.col = collIndex;
+                cellElement.textContent = cell.getMark();
+                boardElement.appendChild(cellElement);
+            });
+        });
+    };
+
+    return {
+        updateScreen,
+    };
+}
+
+ScreenController().updateScreen();
+
+
 
 
 
