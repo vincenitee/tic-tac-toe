fd7e2bbbc2e5bc70b144f142370d3579b7a20d12
Vincent <vincentbolinget@gmail.com>
2025-07-19 22:29:09 -1200

    Add winner modal

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ function Gameboard() {
 
     const boardSize = 3;
 
-    for (let row = 0; row < boardSize; row++) {
-        board[row] = [];
-        for (let col = 0; col < boardSize; col++) {
-            board[row].push(Cell());
+    const generateBoard = () => {
+        for (let row = 0; row < boardSize; row++) {
+            board[row] = [];
+            for (let col = 0; col < boardSize; col++) {
+                board[row].push(Cell());
+            }
         }
-    }
+    };
 
     const getBoard = () => board;
 
@@ -33,12 +35,14 @@ function Gameboard() {
         return true;
     };
 
+    generateBoard();
+
     const printBoard = () => {
         const cellWithMarks = board.map((row) => row.map((cell) => cell.getMark()));
         console.log(cellWithMarks);
     };
 
-    return { addMark, getBoard, getBoardSize, printBoard }
+    return { addMark, getBoard, getBoardSize, printBoard, generateBoard }
 }
 
 // Generates a cell object
@@ -60,17 +64,23 @@ function createPlayer(name, mark) {
 }
 
 // Main controller of the game
-function GameController() {
-    let scores = {X: 0, O: 0, draw: 0};
+function GameController(
+    playerOneName = 'Player 1',
+    playerTwoName = 'Player 2'
+) {
+    let scores = { X: 0, O: 0, draw: 0 };
 
     let gameOver = false;
+
+    let winner = null;
+
     // Initialize board
     const board = Gameboard();
 
     // Initialize players
     const players = [
-        createPlayer('Player 1', 'X'),
-        createPlayer('Player 2', 'O'),
+        createPlayer(playerOneName, 'X'),
+        createPlayer(playerTwoName, 'O'),
     ];
 
     let activePlayer = players[0];
@@ -85,6 +95,8 @@ function GameController() {
 
     const isGameOver = () => gameOver;
 
+    const getWinner = () => winner;
+
     const getBoard = () => board.getBoard();
 
     const playRound = (row, col) => {
@@ -94,20 +106,27 @@ function GameController() {
             const isValidMove = board.addMark(row, col, mark);
 
             if (!isValidMove) {
-                console.log('Invalid move, please try again.');
                 return;
             }
 
-            if(isBoardFull() && !checkWinner(row, col)) {
-                scores['draw'] += 1;
+            if (isBoardFull() && !checkWinner(row, col)) {
+                scores.draw += 1;
+
+                winner = 'draw';
+                
                 gameOver = true;
+                
                 return;
             }
 
             if (checkWinner(row, col)) {
                 scores[mark] += 10;
+
+                winner = mark;
+
                 gameOver = true;
-                return activePlayer;
+                
+                return;
             }
 
             switchActivePlayer();
@@ -151,12 +170,20 @@ function GameController() {
             .every(row => row.every(cell => cell.getMark() !== ''));
     }
 
+    const newGame = () => {
+        gameOver = false;
+        winner = null;
+        board.generateBoard();
+    }
+
     return {
         playRound,
         getActivePlayer,
         getScores,
         getBoard,
         isGameOver,
+        getWinner,
+        newGame,
     };
 
 }
@@ -173,85 +200,122 @@ function ScreenController() {
                     <path
                         d="M13.46,12L19,17.54V19H17.54L12,13.46L6.46,19H5V17.54L10.54,12L5,6.46V5H6.46L12,10.54L17.54,5H19V6.46L13.46,12Z" />
                 </svg>`,
+        'draw' : `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <title>scale-balance</title>
+                    <path
+                        d="M12,3C10.73,3 9.6,3.8 9.18,5H3V7H4.95L2,14C1.53,16 3,17 5.5,17C8,17 9.56,16 9,14L6.05,7H9.17C9.5,7.85 10.15,8.5 11,8.83V20H2V22H22V20H13V8.82C13.85,8.5 14.5,7.85 14.82,7H17.95L15,14C14.53,16 16,17 18.5,17C21,17 22.56,16 22,14L19.05,7H21V5H14.83C14.4,3.8 13.27,3 12,3M12,5A1,1 0 0,1 13,6A1,1 0 0,1 12,7A1,1 0 0,1 11,6A1,1 0 0,1 12,5M5.5,10.25L7,14H4L5.5,10.25M18.5,10.25L20,14H17L18.5,10.25Z" />
+                </svg>`,
     };
 
     const select = (element) => document.querySelector(element);
 
+    const game = GameController();
     const boardElement = select('.game-board');
-    
-    const playerXScoreElement = select('#player-X-score');
-    const playerOScoreElement = select('#player-O-score');
+
+    const board = game.getBoard();
+    const playerXTurn = select('#player-x');
+    const playerOTurn = select('#player-o');
+
+    const playerXScoreElement = select('#player-X-score'); 
+    const playerOScoreElement = select('#player-O-score'); 
     const drawScoreElement = select('#draw-score');
 
-    const playerXElement = select('#player-x');
-    const playerOElement = select('#player-o');
+    const winnerDialog = select('.winner-dialog');
+    const winnerMessageElement = select('.winner-dialog__message');
+    const winnerMarkElement = select('.winner-dialog__mark');
+    const closeDialogBtn = select('.close-dialog');
 
-    const gameController = GameController();
+    const gameControlsElement = select('.game-controls');
+    const newGameBtn = select('.new-game');
+
+    gameControlsElement.classList.toggle('hidden');
+
+    newGameBtn.addEventListener('click', () => {
+        game.newGame();
+        updateScreen();
+    });
+
+    closeDialogBtn.addEventListener('click', () => {
+        winnerDialog.close();
+    });
 
     const updateScreen = () => {
-        const board = gameController.getBoard();
-        const activePlayer = gameController.getActivePlayer();
-        const isGameOver = gameController.isGameOver();
-        
-        if(isGameOver) (activePlayer.mark === 'X' ? playerXElement : playerOElement).classList.add('winner');
+        // Clears the board
+        boardElement.innerHTML = '';
 
-        const {X: playerXScore, O: playerOScore, draw: drawScore } = gameController.getScores();
+        // Switches player's turn
+        const currentPlayer = game.getActivePlayer();
 
+        [playerXTurn, playerOTurn].forEach((el) => el.classList.remove('current'));
+        (currentPlayer.mark === 'X' ? playerXTurn : playerOTurn).classList.add('current');
+
+        // Displays Score
+        const { X: playerXScore, O: playerOScore, draw: drawScore } = game.getScores();
         playerXScoreElement.textContent = playerXScore;
         playerOScoreElement.textContent = playerOScore;
         drawScoreElement.textContent = drawScore;
 
-        [playerXElement, playerOElement].forEach(e => e.classList.remove('current'));
-        (activePlayer.mark === 'X' ? playerXElement : playerOElement).classList.add('current');
-
-        boardElement.innerHTML = '';
-
-        renderBoard(board);
-    };
-
-    const renderBoard = (board) => {
+        // Renders the board
         board.forEach((row, rowIndex) => {
-            row.forEach((cell, collIndex) => {
-                const cellMark = cell.getMark();
-                const isTaken = cellMark !== '';
-
-                const cellElement = document.createElement('button');
-                cellElement.classList.add('cell');
-                cellElement.dataset.row = rowIndex;
-                cellElement.dataset.col = collIndex;
-                cellElement.innerHTML = isTaken ? marks[cellMark] : '';
-
-                if(isTaken) cellElement.classList.add('taken');
-                
-                boardElement.appendChild(cellElement);
+            row.forEach((cell, colIndex) => {
+                const cellButton = createCellButton(cell, rowIndex, colIndex);
+                boardElement.appendChild(cellButton);
             });
         });
+
+        // Checks if the game is over
+        if(game.isGameOver()){
+            const winner = game.getWinner();
+            if(winner === 'draw') {
+                announceWinner(marks[winner], 'TIE')
+            } else {
+                announceWinner(marks[winner], 'Win')
+            }
+        }
+    }
+
+    const announceWinner = (mark, message) => {
+        winnerMarkElement.innerHTML = mark;
+        winnerMessageElement.textContent = message;
+        winnerDialog.showModal();
     };
 
-    const clickHandlerBoard = () => {
-        boardElement.addEventListener('click', (event) => {
-            if(!event.target.classList.contains('cell')) return;
+    const createCellButton = (cell, rowIndex, colIndex) => {
+        const cellMark = cell.getMark();
 
-            const cell = event.target;
-            const cellRow = parseInt(cell.dataset.row);
-            const cellCol = parseInt(cell.dataset.col);
+        const cellButton = document.createElement('button');
+        cellButton.classList.add('cell');
+        cellButton.dataset.row = rowIndex;
+        cellButton.dataset.col = colIndex;
 
-            gameController.playRound(cellRow, cellCol);
-            updateScreen();
-        });
+        const isMarked = cellMark !== '';
+        cellButton.innerHTML = isMarked ? marks[cellMark] : '';
+        if (isMarked) cellButton.classList.add('taken');
 
+        return cellButton;
+    }
+
+    const clickHandlerBoard = (e) => {
+        const selectedCell = e.target;
+
+        if (!selectedCell.classList.contains('cell')) return;
+
+        const cellCol = parseInt(selectedCell.dataset.col);
+        const cellRow = parseInt(selectedCell.dataset.row);
+
+        game.playRound(cellRow, cellCol);
+    
         updateScreen();
     };
 
-    return {
-        updateScreen,
-        clickHandlerBoard
-    };
+    boardElement.addEventListener('click', clickHandlerBoard);
+
+    // Initial render
+    updateScreen();
 }
 
-(function(){
-    const screenController = ScreenController();
-    screenController.clickHandlerBoard();
+(function () {
+    ScreenController();
 })();
 
 
